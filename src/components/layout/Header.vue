8646e5d53421f7403e26e0f9b4b87309e413f793
zhangyanxin333 <15980368907@163.com>
2018-11-02 22:51:42 +0800

    头部尾部的构建
    Change-Id: I9f3a3c8975b99a3f91503695fe679750a5f52c1e

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        猫眼电影
+    </div>
+</template>
+
+<style lang="stylus" scoped>
+@import '~styles/variables.styl'
+    div 
+        height .505rem
+        background $base-color
+        font-size .18rem
+        color white
+        line-height .505rem
+        text-align center
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="wrap">
+        <Header></Header>
+        <main></main>
+        <TabBar></TabBar>
+    </div>
+</template>
+
+<script>
+    import Header from 'components/layout/Header'
+    import TabBar from 'components/layout/TabBar'    
+    export default {
+        components: {
+            Header,
+            TabBar
+        }
+    }
+</script>
+<style lang="stylus" scoped>
+    .wrap 
+        height 100%
+        display flex
+        flex-direction column
+        main 
+            display flex
+            flex 1
+</style>
+
+
